36850c9fa396d27297a1e3687c15d5d68688bbf8
xuyang <xuyangme@126.com>
2019-04-10 17:21:57 +0800

    【refactor】

---
+++ lib_autotrack/src/main/java/k/xy/lib_autotrack/util/LogTag.java
@@ -0,0 +1,9 @@
+package k.xy.lib_autotrack.util;
+
+public class LogTag {
+
+    public static final String TAG_ASPECTJ = "tag_aspectj";
+    public static final String TAG_ACTIVITY = "tag_activity";
+    public static final String TAG_VIEW_CLICK = "tag_view_click";
+
+}
--- myaspectj_lib1/src/main/java/k/xy/lib_gintonic/LogTag.java
+++
@@ -1,8 +0,0 @@
-package k.xy.lib_gintonic;
-
-public class LogTag {
-
-    public static final String TAG_ACTIVITY = "tag_activity";
-    public static final String TAG_VIEW_CLICK = "tag_view_click";
-
-}
